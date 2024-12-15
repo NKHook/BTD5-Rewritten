c9fc36fb45d6412f81c35b5d3a5081e7c1a32e03
Mike <mallisiscool@gmail.com>
2024-12-15 13:09:29 -0500

    Actually render at window resolution lol

--- godot/Tools/CompoundSprite/CsEditorZone.cs
+++ godot/Tools/CompoundSprite/CsEditorZone.cs
@@ -1,8 +1,18 @@
 using Godot;
-using System;
 using BloonsTD5Rewritten.NewFramework.Scripts.Compound;
 
 public partial class CsEditorZone : HSplitContainer
 {
 	[Export] public CompoundSprite? PreviewSprite;
+	[Export] public SubViewport? PreviewViewport;
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		if (PreviewSprite == null)
+			return;
+		var resolution = PreviewViewport?.Size ?? Vector2.Zero;
+		PreviewSprite!.Position = resolution * 0.5f;
+	}
 }
